0972a0d3707afb775b09cbdcfd5fa935b808d897
Jonathan Søgaard Petersen <jona173a@edu.easj.dk>
2019-02-01 13:50:31 +0100

    Tilføjet FlatWhite klassen

---
+++ KaffeConsoleAppNetCore/FlatWhite.cs
@@ -0,0 +1,30 @@
+﻿using kaffe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaffeConsoleAppNetCore
+{
+    public class FlatWhite : Kaffe , Imælk
+    {
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public override int Pris()
+        {
+            return 45;
+        }
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+
+        public FlatWhite (int rabat) : base(rabat)
+        {
+        }
+    }
+}
--- KaffeConsoleAppNetCore/Program.cs
+++ KaffeConsoleAppNetCore/Program.cs
@@ -1,5 +1,7 @@
 ﻿using kaffe;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace KaffeConsoleAppNetCore
 {
@@ -9,9 +11,29 @@ namespace KaffeConsoleAppNetCore
         {
             Console.WriteLine("Hello World!");
 
-            
+            Cortado cortado = new Cortado();
+            Latte latte = new Latte();
+            SortKaffe sortKaffe = new SortKaffe();
+            FlatWhite flatWhite = new FlatWhite(12);
 
-           
+            List<Imælk> liste = new List<Imælk>();
+            liste.Add(cortado);
+            liste.Add(latte);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine("Før cast" + item.MlMælk());
+
+                if (item is Latte)
+                {
+                    Latte 1 = item as Latte;
+                    Console.WriteLine("Latte styrker" + 1.Styrke());
+                }
+                else if (item is Cortado)
+                {
+                    Cortado c = item as Cortado;
+                }
+            }
 
         }
     }
